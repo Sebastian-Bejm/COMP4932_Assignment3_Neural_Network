381f3327b49950687c76de081ac6eb1d09b8baf1
Rammel Razon <rammel1012@gmail.com>
2020-04-04 18:14:36 -0700

    created file structure and nuget package numsharp and kera.net for numpy and theno alternative

---
+++ NeuralNet/fig/replaced_by_d3/tanh.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNet.fig.replaced_by_d3
+{
+    class tanh
+    {
+    }
+}
