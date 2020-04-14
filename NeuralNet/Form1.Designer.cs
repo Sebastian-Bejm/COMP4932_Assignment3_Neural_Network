3391a9f057d5cbfb006f58760ce9031d1f9804ca
Rammel Razon <rammel1012@gmail.com>
2020-04-13 23:20:54 -0700

    added drawing

--- NeuralNet/Form1.Designer.cs
+++ NeuralNet/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace NeuralNet
+﻿using System.Windows.Forms;
+
+namespace NeuralNet
 {
     partial class Form1
     {
@@ -28,20 +30,207 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.result = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.result)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(739, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Epochs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(739, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "mini batch size";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(739, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Learning Rate";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(743, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(59, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(743, 165);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(59, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(743, 223);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(59, 26);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(633, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Network";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(739, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 24);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(779, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 24);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(33, 616);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 52);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Predict";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(190, 475);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 20);
+            this.label5.TabIndex = 12;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(743, 272);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 28);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Train";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(37, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 31);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.clear);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBox1.Location = new System.Drawing.Point(33, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(560, 560);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_Mouse_Down);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_Mouse_Move);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_Mouse_Up);
+            // 
+            // result
+            // 
+            this.result.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.result.Location = new System.Drawing.Point(637, 362);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(200, 200);
+            this.result.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.result.TabIndex = 16;
+            this.result.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 673);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Neural Network";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.result)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private PictureBox result;
     }
 }
 
--- NeuralNet/Form1.cs
+++ NeuralNet/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,27 +16,67 @@ namespace NeuralNet
 {
     public partial class Form1 : Form
     {
+        Point lastPoint = Point.Empty;
+        bool isMouseDown = new bool();
+
         mnist_loader ml = new mnist_loader();
 
         public Form1()
         {
             InitializeComponent();
-            Tuple<List<Tuple<NDArray, NDArray>>, List<Tuple<NDArray, NDArray>>, List<Tuple<NDArray, NDArray>>> tuple = ml.load_data_wrapper(); // testing for now
+            //Tuple<List<Tuple<NDArray, NDArray>>, List<Tuple<NDArray, NDArray>>, List<Tuple<NDArray, NDArray>>> tuple = ml.load_data_wrapper(); // testing for now
 
-            List<Tuple<NDArray, NDArray>> training_data = tuple.Item1;
-            List<Tuple<NDArray, NDArray>> validation_data = tuple.Item2;
-            List<Tuple<NDArray, NDArray>> test_data = tuple.Item3;
+            //List<Tuple<NDArray, NDArray>> training_data = tuple.Item1;
+            //List<Tuple<NDArray, NDArray>> validation_data = tuple.Item2;
+            //List<Tuple<NDArray, NDArray>> test_data = tuple.Item3;          
 
-            //Console.WriteLine(test_data[0].Item1.Shape);
-            //Console.WriteLine(test_data[0].Item2.Shape);
+            //network network = new network(new int[] { 784, 30, 10 });
+            //network.SGD(training_data, 30, 10, 3.0, test_data);
+        }
 
+        private void pictureBox1_Mouse_Down(object sender, MouseEventArgs e)
+        {
+            lastPoint = e.Location;
+            isMouseDown = true;
+        }
 
-            //Console.WriteLine(training_data.Count);
-            //Console.WriteLine(validation_data.Count);
-            //Console.WriteLine(test_data.Count);            
+        private void pictureBox1_Mouse_Move(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown == true) {
+                if (lastPoint != null) {
+                    if (pictureBox1.Image == null) {
+                        Bitmap bmp = new Bitmap(560, 560);
+                        pictureBox1.Image = bmp;
+                    }
+                    using (Graphics g = Graphics.FromImage(pictureBox1.Image)) {
+                        g.DrawLine(new Pen(Color.Black, 2), lastPoint, e.Location);
+                        g.SmoothingMode = SmoothingMode.AntiAlias;
+                    }
+                    pictureBox1.Invalidate();
+                    lastPoint = e.Location;
+                }
+            }
+        }
 
-            network network = new network(new int[] { 784, 30, 10 });
-            network.SGD(training_data, 30, 10, 3.0, test_data);
+        private void pictureBox1_Mouse_Up(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+            lastPoint = Point.Empty;
+        }
+
+        private void clear(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null) {
+                pictureBox1.Image = null;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap res = new Bitmap(pictureBox1.Image,pictureBox1.Image.Width / 10 / 2, pictureBox1.Height / 10 / 2);
+            result.Image = res;
+            result.Invalidate();
         }
     }
 }
